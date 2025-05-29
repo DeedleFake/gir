3f9a9f91368343ad6ac82f39335dca4e6dfe2bf5
DeedleFake <deedlefake@users.noreply.github.com>
2025-05-29 12:03:03 -0400

    internal/util: add `JoinSeq()` and `JoinPairs()`

--- internal/util/iter.go
+++ internal/util/iter.go
@@ -2,8 +2,11 @@ package util
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"iter"
+	"slices"
+	"strings"
 )
 
 func Pairs[T any](seq iter.Seq[T]) iter.Seq2[T, T] {
@@ -38,4 +41,20 @@ func Lines(r io.Reader) iter.Seq2[string, error] {
 			yield("", s.Err())
 		}
 	}
+}
+
+func JoinSeq[T any](seq iter.Seq[T], sep string) string {
+	return strings.Join(slices.Collect(func(yield func(string) bool) {
+		for v := range seq {
+			yield(fmt.Sprint(v))
+		}
+	}), sep)
+}
+
+func JoinPairs[T1, T2 any](seq iter.Seq2[T1, T2], psep, sep string) string {
+	return strings.Join(slices.Collect(func(yield func(string) bool) {
+		for v1, v2 := range seq {
+			yield(fmt.Sprintf("%v%v%v", v1, psep, v2))
+		}
+	}), sep)
 }
